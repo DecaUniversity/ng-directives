22e4ed7f1e34dd1b78678d614ce0226d33ba553f
DecaHub <dan@decahub.com>
2016-10-04 18:34:42 -0400

    First Directive

---
+++ app.js
@@ -0,0 +1,13 @@
+"use strict";
+
+angular.module("app", []);
+
+angular.module("app").controller("MainController", ["$scope", function () {
+	
+}]);
+
+angular.module("app").directive("userInfoCard", function () {
+	return {
+		template: "Hello, User!"
+	}
+});
